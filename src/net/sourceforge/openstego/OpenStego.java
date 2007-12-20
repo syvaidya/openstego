758a28bed5eaa23a444327d5bdd66d34c7b4b422
Samir Vaidya <syvaidya@users.noreply.github.com>
2007-12-20 09:49:04 +0000

    Changed the window icon

--- src/net/sourceforge/openstego/OpenStego.java
+++ src/net/sourceforge/openstego/OpenStego.java
@@ -11,7 +11,6 @@ import net.sourceforge.openstego.util.LabelUtil;
 
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.util.Arrays;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.zip.GZIPInputStream;
